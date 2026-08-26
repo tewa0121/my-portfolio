57158f4f1857b7197e7309bde104e35d10ad76c8
tewa0121 <tewachewmelaku6@gmail.com>
2026-08-26 21:45:58 +0300

    dfgh

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,6 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     {/* <ErrorBoundary> */}
       <App />
-    </ErrorBoundary>
+    {/* </ErrorBoundary> */}
   </React.StrictMode>
 );
