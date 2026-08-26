dbbc10ccc9f4637074a3c4b40605c2506acdb6e4
tewa0121 <tewachewmelaku6@gmail.com>
2026-08-26 21:54:04 +0300

    jhg

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,11 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
-import About from "./components/About";
-import Skills from "./components/Skills";
-import Projects from "./components/Projects";
-import Contact from "./components/Contact";
-import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
-      <About />
-      <Skills />
-      {/* <Projects /> */}    {/* ← Comment out Projects */}
-      {/* <Contact /> */}     {/* ← Comment out Contact */}
-      {/* <Footer /> */}      {/* ← Comment out Footer */}
     </>
   );
 }
