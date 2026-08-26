9d8c684854d6d4f0340af3fbe6a61b7d95535416
tewa0121 <tewachewmelaku6@gmail.com>
2026-08-26 22:54:33 +0300

    kjhgf

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
-// import About from "./components/About";
+import About from "./components/About";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
@@ -11,7 +11,7 @@ function App() {
     <>
       <Navbar />
       <Hero />
-      {/* <About /> */}
+      <About />
       <Skills />
       <Projects />
       <Contact />
